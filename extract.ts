36027372568763677d77541a9ec4213b87558947
aleksanderwalczuk <ohleg@windowslive.com>
2024-09-04 02:18:22 +0200

    mv rate limit headers getter to extract

--- extract.ts
+++ extract.ts
@@ -1,3 +1,5 @@
+import { Headers } from "openai/_shims/index.mjs";
+
 export const extractI18nContent = (content: string): string | null => {
   const i18nStart = '<i18n lang="yaml">';
   const i18nEnd = "</i18n>";
@@ -31,4 +33,57 @@ export const extractI18nIndexes = (
     start: startIndex + '<i18n lang="yaml">'.length + 1,
     end: endIndex,
   };
+};
+
+/**
+ * Sample headers:
+ * {
+ *  "remaining-tokens":"59905",
+ *  "remaining-requests":"197",
+ *  "reset-tokens":"95ms",
+ *  "reset-requests":"21m35.083s"
+ * }
+ */
+const parseRateLimitHeaders = (headers: Record<string, string> | null) => {
+  const keyParser = {
+    "remaining-tokens": parseInt,
+    "remaining-requests": parseInt,
+    "reset-tokens": String,
+    "reset-requests": String,
+  };
+  let parsed = {};
+  for (const key in headers) {
+    if (Object.prototype.hasOwnProperty.call(headers, key)) {
+      const element = headers[key];
+      parsed = {
+        ...parsed,
+        [key]: keyParser[key as keyof typeof keyParser](element),
+      };
+    }
+  }
+
+  return parsed as {
+    "remaining-tokens": number;
+    "remaining-requests": number;
+    "reset-tokens": string;
+    "reset-requests": string;
+  };
+};
+
+export const extractRateLimitHeaders = (headers: Headers) => {
+  const rateLimitHeaders = [
+    "x-ratelimit-remaining-tokens",
+    "x-ratelimit-remaining-requests",
+    "x-ratelimit-reset-tokens",
+    "x-ratelimit-reset-requests",
+  ];
+
+  return parseRateLimitHeaders(
+    Object.fromEntries(
+      rateLimitHeaders.map((key) => [
+        key.slice("x-ratelimit-".length),
+        headers.get(key)!,
+      ])
+    )
+  );
 };
--- translate.ts
+++ translate.ts
@@ -1,29 +1,5 @@
 import { type OpenAI } from "openai";
-import { Headers } from "openai/_shims/node-types.mjs";
-
-const getRateLimitHeaders = (headers: Headers) => {
-  const rateLimitHeaders = [
-    "x-ratelimit-remaining-tokens",
-    "x-ratelimit-remaining-requests",
-    "x-ratelimit-reset-tokens",
-    "x-ratelimit-reset-requests",
-  ];
-  /**
-   * Sample response:
-   * {
-   *  "remaining-tokens":"59905",
-   *  "remaining-requests":"197",
-   *  "reset-tokens":"95ms",
-   *  "reset-requests":"21m35.083s"
-   * }
-   */
-  return Object.fromEntries(
-    rateLimitHeaders.map((key) => [
-      key.slice("x-ratelimit-".length),
-      headers.get(key),
-    ])
-  );
-};
+import { extractRateLimitHeaders } from "./extract";
 
 export const translate = async (
   content: string,
@@ -50,7 +26,7 @@ export const translate = async (
 
   const [choice] = response.data.choices;
 
-  const rateLimits = getRateLimitHeaders(response.response.headers);
+  const rateLimits = extractRateLimitHeaders(response.response.headers);
 
   if (choice.finish_reason !== "stop") {
     throw new Error("Something went wrong," + choice.message.refusal);
